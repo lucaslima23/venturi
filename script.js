09d93302f8b587aa0dd7b86d79e9afb3bdeec6c0
lucaslima23 <30532330+lucaslima23@users.noreply.github.com>
2025-08-19 20:16:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Registra o Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('Service Worker registrado com sucesso:', registration.scope);
             })
@@ -182,4 +182,5 @@ function displayResults(score1, score2) {
         `;
     }
 }
+
 
